2934dca8e5103dd77354d91eb3c36bce2dd9658e
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-06-19 15:02:20 -0700

    Init

---
+++ models/User.php
@@ -0,0 +1,33 @@
+<?php 
+    class User {
+        private $id;
+        private $email;
+        private $password;
+        private $role;
+
+        public function getId(){
+            return $this->id;
+        }
+        public function setId($id){
+            $this->id = $id;
+        }
+        public function getEmail(){
+            return $this->email;
+        }
+        public function setEmail($email){
+            $this->email = $email;
+        }
+        public function getPassword(){
+            return $this->password;
+        }
+        public function setPassword($pass){
+            $this->password = $pass;
+        }
+        public function getRole(){
+            return $this->role;
+        }
+        public function setRole($role){
+            $this->role = $role;
+        }
+    }
+?>
---
+++ university.sql
@@ -0,0 +1,297 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-06-2023 a las 23:47:53
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `university`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `DNI` varchar(60) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `birthday` varchar(255) NOT NULL,
+  `id_user` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id_curso` int(11) UNSIGNED NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `id_maestro` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso_alumno`
+--
+
+CREATE TABLE `curso_alumno` (
+  `id_curso_alumno` int(11) NOT NULL,
+  `id_curso` int(11) UNSIGNED NOT NULL,
+  `DNI` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `maestro`
+--
+
+CREATE TABLE `maestro` (
+  `id_maestro` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `birthday` varchar(20) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `id_user` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `id_permiso` int(10) UNSIGNED NOT NULL,
+  `accion` varchar(45) NOT NULL,
+  `target` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `permisos`
+--
+
+INSERT INTO `permisos` (`id_permiso`, `accion`, `target`) VALUES
+(1, 'ADD', 'USER'),
+(2, 'UPDATE', 'USER'),
+(3, 'DELETE', 'USER'),
+(4, 'ADD', 'CURSO'),
+(5, 'DELETE', 'CURSO');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_role` int(10) UNSIGNED NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_role`, `name`) VALUES
+(1, 'Administrador'),
+(2, 'Maestro'),
+(3, 'Estudiante');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `role_permiso`
+--
+
+CREATE TABLE `role_permiso` (
+  `id_role_permiso` int(10) UNSIGNED NOT NULL,
+  `id_role` int(10) UNSIGNED NOT NULL,
+  `id_permiso` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(10) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `id_role` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id_user`, `email`, `password`, `id_role`) VALUES
+(1, 'admin@admin', 'admin', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`DNI`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indices de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id_curso`),
+  ADD KEY `id_maestro` (`id_maestro`);
+
+--
+-- Indices de la tabla `curso_alumno`
+--
+ALTER TABLE `curso_alumno`
+  ADD KEY `id_curso` (`id_curso`,`DNI`),
+  ADD KEY `DNI` (`DNI`);
+
+--
+-- Indices de la tabla `maestro`
+--
+ALTER TABLE `maestro`
+  ADD PRIMARY KEY (`id_maestro`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indices de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`id_permiso`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Indices de la tabla `role_permiso`
+--
+ALTER TABLE `role_permiso`
+  ADD PRIMARY KEY (`id_role_permiso`),
+  ADD KEY `id_role` (`id_role`,`id_permiso`),
+  ADD KEY `id_permiso` (`id_permiso`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_role` (`id_role`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id_curso` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `maestro`
+--
+ALTER TABLE `maestro`
+  MODIFY `id_maestro` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `id_permiso` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_role` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `role_permiso`
+--
+ALTER TABLE `role_permiso`
+  MODIFY `id_role_permiso` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD CONSTRAINT `alumnos_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`id_maestro`) REFERENCES `maestro` (`id_maestro`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `curso_alumno`
+--
+ALTER TABLE `curso_alumno`
+  ADD CONSTRAINT `curso_alumno_ibfk_1` FOREIGN KEY (`DNI`) REFERENCES `alumnos` (`DNI`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `curso_alumno_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `maestro`
+--
+ALTER TABLE `maestro`
+  ADD CONSTRAINT `maestro_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `role_permiso`
+--
+ALTER TABLE `role_permiso`
+  ADD CONSTRAINT `role_permiso_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id_role`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `role_permiso_ibfk_2` FOREIGN KEY (`id_permiso`) REFERENCES `permisos` (`id_permiso`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id_role`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
